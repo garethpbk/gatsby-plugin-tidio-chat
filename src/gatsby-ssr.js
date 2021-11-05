f7e0c450bb6657d74e6c0f1290796afb07db8140
vishnumaitai <vishnu@maitaitech.com>
2021-11-05 10:38:01 +0530

    window issue test6

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -4,6 +4,12 @@ exports.onRenderBody = (
   { setHeadComponents },
   { tidioKey, enableDuringDevelop = true, delayInMilliseconds = 0 },
 ) => {
+
+  if (delayInMilliseconds > 0) {
+    setTimeout(function () {
+      global.window.tidioChatApi.open();
+    }, delayInMilliseconds * 1000);
+  }
   if (!enableDuringDevelop && process.env.NODE_ENV === 'development') {
     console.log(
       'enableDuringDevelop is set to false - gatsby-plugin-tidio-chat will not load in development mode',
@@ -24,14 +30,6 @@ exports.onRenderBody = (
       key="gatsby-plugin-tidio-chat"
       src={`//code.tidio.co/${tidioKey}.js`}
       async
-    />, <script>
-      {delayInMilliseconds > 0 && (
-        setTimeout(function () {
-          window.tidioChatApi.open();
-        }, delayInMilliseconds * 1000)
-      )}
-    </script>,
-
-
+    />,
   ])
 }
